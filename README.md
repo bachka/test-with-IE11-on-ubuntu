92e5461d7c5cdabc3d8919ff30a0f0edb82ace52
bachka <bachka@intrafind.de>
2019-01-14 17:24:02 +0100

    adding readme and fixing bug

---
+++ README.md
@@ -0,0 +1,10 @@
+So what ist done here:
+- 
+1. Download virtualbox image with IE11 on Windows 7 (~ 5GiB)
+2. Unzip the official image from MS
+3. Install virtualbox and virtualbox extensions
+4. Import image to virtualbox (25-30 GiB)
+5. Create snapshot
+6. User: IE11 and Password: Passw0rd!
+Use the snapshot to start again after 90 days trial.
+This is at the moment the best way to be shure that you are really testing on IE11.
--- install-ie11-on-linux.sh
+++ install-ie11-on-linux.sh
@@ -7,7 +7,7 @@ sudo apt-get install zip unzip -y
 cd /home/$USER/Downloads
 curl https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win7.VirtualBox.zip --output IE11.Win7.VirtualBox.zip
 unzip  IE11.Win7.VirtualBox.zip
-sudo apt-get install virtualbox install virtualbox-ext-pack -y
+sudo apt-get install virtualbox virtualbox-ext-pack -y
 vboxmanage import 'IE11 - Win7.ova'
 vboxmanage snapshot 'IE11 - Win7' take "Snapshot taken on $NOW" --description 'This virtual machine expires after 90 days after activation. We did a snapshot before, just use it to roll back to before activation. The password to your VM is "Passw0rd!". Finished to install Virtualbox! Start your VM, install on Windows Virtualbox Extension, Update Windows and install Security Essentials then you are ready to test with IE11! After all shutdown VM and increase Video Memory at least to 64 MiB (for good full screen resolution).'
 rm "IE11 - Win7.ova"
